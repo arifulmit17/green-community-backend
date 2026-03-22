0e807e487c2ae56c382a14675a8070f657352977
Ariful <arifulmit17@gmail.com>
2026-03-22 21:59:58 +0600

    added auth module

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,60 @@
+
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import jwt from "jsonwebtoken";
+
+
+const secret="ssdfsfsdfsdfsfsfsdfgswrwer"
+
+const  createUserAuth= async(payload:any)=>{
+    
+    const {password}=payload
+     console.log(payload);
+    const hashedPass=await bcrypt.hash(password, 8);
+    const result=await prisma.user.create({
+        data:{
+            ...payload,
+            password:hashedPass
+        }
+    })
+    
+    return result
+}
+
+
+const loginUserAuth=async (payload:any)=>{
+   const user=await prisma.user.findUnique({
+    where:{
+        email:payload.email
+    }
+    
+    
+   
+   })
+   if(!user){
+        throw new Error("User not found")
+    }
+    const verifypass=await bcrypt.compare(payload.password,user.password)
+    if(!verifypass){
+        throw new Error("Invalid credential")
+    }
+
+    const userData={
+        id:user.id,
+        name:user.name,
+        email:user.email,
+        role:user.role,
+    }
+    const token=jwt.sign(userData,secret,{expiresIn:"3d"})
+
+    return {
+        token,
+        user
+    }
+}
+
+export const AuthService = {
+    // Add service methods here
+      createUserAuth,
+      loginUserAuth
+    };
