2f2c359696da7fc168ee5f24075b0a8e0ec8c1ae
Ariful <arifulmit17@gmail.com>
2026-04-12 22:44:19 +0600

    changed idearouter get api security

--- src/modules/idea/idea.router.ts
+++ src/modules/idea/idea.router.ts
@@ -9,7 +9,7 @@ router.post("/",authMiddleware, ideaController.createIdea);
 
 
 
-router.get("/:id",authMiddleware, ideaController.getIdeaById);
+router.get("/:id", ideaController.getIdeaById);
 router.get("/user/:userId",authMiddleware, ideaController.getIdeasByUserId);
 
 router.patch("/:id",authMiddleware, ideaController.updateIdea);
